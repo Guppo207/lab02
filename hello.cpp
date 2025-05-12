f3b7409e5faa63f102adfc4e4d0ce78ac1cb3244
Guppo207 <kirill.sherstyuk06@mail.ru>
2025-05-12 10:11:06 +0000

    Добавление комментариев

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,12 @@
+//Подключение библиотек
 #include <iostream>
 #include <string>
 int main(){
+	//Введение переменной name
 	std::string name;
+	//Ввод значения name
 	std::cin>>name;
+	//Вывод сообщения на экран
 	std::cout<<"Hello world form "<<name<<std::endl;
 	
 	return 1;
