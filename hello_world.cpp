735bb1fd669dd9d6b499cd48aa5ef96d44b6872c
Guppo207 <kirill.sherstyuk06@mail.ru>
2025-05-23 06:59:07 +0000

    comment change

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 int main(){
 	std::string name;//имя
-	std::cin>>name;
-	std::cout<<"Hello world from "<<name<<std::endl;//Вывод
+	std::cin>>name;//Ввод имени
+	std::cout<<"Hello world from "<<name<<std::endl;//Вывод на экран
 	return 1;
 }
