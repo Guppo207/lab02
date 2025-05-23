97600a33fbc35a674ecb9923aded87fa79eb72f0
Guppo207 <kirill.sherstyuk06@mail.ru>
2025-05-23 10:02:04 +0000

    chenged style 'hello_world.cpp'

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <string>
-int main(){
-	std::string name;//имя
-	std::cin>>name;//Ввод имени
-	std::cout<<"Hello world from "<<name<<std::endl;//Вывод на экран
-	return 1;
+using namespace std;
+int
+main()
+{
+  string name; // имя
+  cin >> name;
+  cout << "Hello world from " << name << endl; // Вывод
+  return 1;
 }
