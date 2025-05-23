e602b7177482241651e8b65ca1d8251d0336a044
Guppo207 <kirill.sherstyuk06@mail.ru>
2025-05-23 06:56:04 +0000

    fix programm

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <string>
-using namespace std;
 int main(){
-	string name;//имя
-	cin>>name;
-	cout<<"Hello world from "<<name<<endl;//Вывод
+	std::string name;//имя
+	std::cin>>name;
+	std::cout<<"Hello world from "<<name<<std::endl;//Вывод
 	return 1;
 }
